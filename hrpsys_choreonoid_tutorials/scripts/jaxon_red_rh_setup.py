386c4dcfd97277b43222051a38d458b6bb0b404a
Kazuki Fukazawa <fukazawa@jsk.imi.i.u-tokyo.ac.jp>
2019-12-14 16:41:49 +0900

    to connect vfs value  ;;debug right now

--- hrpsys_choreonoid_tutorials/scripts/jaxon_red_rh_setup.py
+++ hrpsys_choreonoid_tutorials/scripts/jaxon_red_rh_setup.py
@@ -52,8 +52,8 @@ class JAXON_RED_HrpsysConfigurator(ChoreonoidHrpsysConfigurator):
         # Suppress limit over message and behave like real robot that always angle-vector is in seq.
         # Latter four 0.0 are for hands.
         self.seq_svc.setJointAngles(self.jaxonResetPose()+[0.0, 0.0, 0.0, 0.0] , 1.0)
-        self.ic_svc.startImpedanceController("larm")
-        self.ic_svc.startImpedanceController("rarm")
+        self.ic_svc.startImpedanceController("larm")##########
+        self.ic_svc.startImpedanceController("rarm")##########
         self.startStabilizer()
 
 if __name__ == '__main__':
